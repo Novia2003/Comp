e9325037eb1f69018029af540f51ec3a21391a0b
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-07-03 13:16:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+from src.AST.Parser import *
+from src.Semantics.semantic_analyzer import *
+from src.Сompiler.code_generator import *
+from src.VM.VirtualMachine import *
+
+# строка с кодом, который в последствии будет распознаваться парсером.
+prog = '''
+    var i = 5;
+    do {
+        if(i >= 7)
+            logprint(i);
+        i++;
+    } while(i < 10)
+'''
+# вызов конструктора класса Parser.
+parser = Parser()
+# вызов конструктора класса Analyzer.
+analyzer = Analyzer()
+# в переменной res хранится корень графа, описывающего структуру кода из переменной prog.
+res = parser.parse(prog)
+print(*res.tree, sep=os.linesep)
+
+# вызов метода analyze, который производит семантический анализ
+analyzer.analyze(res)
+if len(analyzer.errors) > 0:
+    for e in analyzer.errors:
+        print("Ошибка: {}".format(e.message))
+else:
+    print("Ошибок не обнаружено.")
+    generator = CodeGenerator(res)
+    generator.print_bytecode()
+    vm = VirtualMachine(generator.lines)
